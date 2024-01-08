cf674ae20436e11f717d4347915c62905d7ed302
LisaPost <yelyzaveta.postnova@testpro.io>
2024-01-08 14:22:26 -0500

    HW18-play-a-song

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,11 +1,63 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
-public class BaseTest {
+import java.time.Duration;
 
+public class BaseTest {
+    WebDriver driver;
+    String url = "https://qa.koel.app/";
     @BeforeSuite
     static void setupClass() {
         WebDriverManager.chromedriver().setup();
     }
+
+    @BeforeMethod
+    public void launchBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+    public void navigateToLoginPage() {
+        driver.get(url);
+    }
+    public void provideEmail(String username) {
+        WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
+        emailField.clear();
+        emailField.sendKeys(username);
+    }
+    public void providePassword(String password) {
+        WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+    public void login() {
+        WebElement submit = driver.findElement(By.cssSelector("button[type='submit']"));
+        submit.click();
+    }
+    public void playNextSong() {
+        Actions actions = new Actions(driver);
+        WebElement playButton = driver.findElement(By.cssSelector("span .play i"));
+        actions.moveToElement(playButton).perform();
+        playButton.click();
+
+        WebElement playNext = driver.findElement(By.xpath("//i[@data-testid='play-next-btn']"));
+        playNext.click();
+    }
+
+    @AfterMethod
+    public void closeBrowser() {
+        driver.quit();
+    }
+
 }
